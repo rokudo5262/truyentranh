b752e4523fec1e89591ad828e047a6fde8279aef
Trump Trinh <trump@reputable.asia>
2021-05-11 19:12:36 +0700

    adding bcrypt

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','upload','session','cart','pagination','image_lib','form_validation');
+$autoload['libraries'] = array('database','upload','session','cart','pagination','image_lib','form_validation','bcrypt');
 
 /*
 | -------------------------------------------------------------------
